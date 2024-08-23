d3802e822ae33b0016685493c5a12818b54d12a7
GoatGear <edgrdoierr@gmail.com>
2024-08-23 17:52:16 -0600

    CORS

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: 'https://api.eleddie.com/api',
-    withCredentials: true
+    // withCredentials: true
 })
 
 export default instance
