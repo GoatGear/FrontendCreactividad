dedb4a96a9340b7cb989ce585f41d8cd7bc34611
GoatGear <edgrdoierr@gmail.com>
2024-08-23 19:14:58 -0600

    Local 100

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: 'https://api.eleddie.com/api',
 });
 
 instance.interceptors.request.use(
